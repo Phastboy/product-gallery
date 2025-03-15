508d3ce1fb75011d9148bd7381988ae35547577e
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-03-15 04:25:03 +0100

    simple navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router";
+
 const Home = () => {
+  const nav = useNavigate();
   return <view>
     <text>This is home</text>
+    <text bindtap={() => nav("/")}>back</text>
   </view>
 }
 export default Home;
